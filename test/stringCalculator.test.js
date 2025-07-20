37b9dfed02291b27f0d038867dfcda2ba28392a4
ankita chavda <a.ankita.chavda@gmail.com>
2025-07-20 15:33:39 +0530

    added failing testcase for single number

--- test/stringCalculator.test.js
+++ test/stringCalculator.test.js
@@ -7,3 +7,7 @@ test('returns 0 for an empty string', () => {
 test('returns 0 for an empty string with space', () => {
   expect(add(" ")).toBe(0);
 });
+
+test("returns number itself when single number is passed", () => {
+    expect(add("1")).toBe(1);
+});
