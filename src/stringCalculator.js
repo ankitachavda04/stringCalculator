a090f795f9eceb36dcbf04038537086fe2e2788f
ankita chavda <a.ankita.chavda@gmail.com>
2025-07-20 17:43:32 +0530

    added logic to handle addition of comma  separated numbers

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,8 +1,12 @@
 export default function add(numbers) {
     if (!numbers || numbers.trim() === "") return 0;
-    return parseInt(numbers);
+    const sum = numbers.split(",")
+                .map(Number)
+                .reduce((acc, curr) => acc + curr, 0);
+    return sum;
 }
 
 console.log(add(""));
 console.log(add(" "));
 console.log(add("10"));
+console.log(add("10,10"));
