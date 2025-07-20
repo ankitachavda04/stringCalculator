25e223b006f88e95531894522f470c38108c5765
ankita chavda <a.ankita.chavda@gmail.com>
2025-07-20 21:56:11 +0530

    added logic to support different delimiters

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,6 +1,12 @@
 export default function add(numbers) {
     if (!numbers || numbers.trim() === "") return 0;
-    const sum = numbers.split(/,|\n/)
+    let delimiters = /,|\n/;
+    if (numbers.startsWith("//")) {
+        [delimiters, numbers] = numbers.split("\n");
+        delimiters = new RegExp(`[${delimiters.slice(2)}]`);
+    }
+    //const numbersWithoutDelimiters = numbers.split(delimiters);
+    const sum = numbers.split(delimiters)
                 .map(Number)
                 .reduce((acc, curr) => acc + curr, 0);
     return sum;
@@ -11,3 +17,4 @@ console.log(add(" "));
 console.log(add("10"));
 console.log(add("10,10"));
 console.log(add("10\n20,30\n40"));
+console.log(add("//?;\n10;20"));
