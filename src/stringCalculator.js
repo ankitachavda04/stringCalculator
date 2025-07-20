4e89457b7ad0e0f4ea3cf3a40e64675a73c50818
ankita chavda <a.ankita.chavda@gmail.com>
2025-07-20 16:20:54 +0530

    handle single number input

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,6 +1,8 @@
 export default function add(numbers) {
     if (!numbers || numbers.trim() === "") return 0;
+    return parseInt(numbers);
 }
 
 console.log(add(""));
 console.log(add(" "));
+console.log(add("10"));
